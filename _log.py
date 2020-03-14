2fa239bce99bf06294f56213a97c5ea777a0f67e
Michalel Menache <62164091+mwmenache@users.noreply.github.com>
2020-03-14 14:56:29 +1100

    update docstring
    Updated the docstring (better example, fixed typos)

--- _log.py
+++ _log.py
@@ -3,14 +3,14 @@ def _log(thing, logfile):
     """ logs to a file 
     
     _log is nothing but the print() function in which what is printed is redirected to a file.
-    This is used for logging: if you run a loing code, you can use _log() to print whatever checks are required
-    to ensure the code ran correctly in a file.
+    This is used for logging: if you run a code, you can use _log() to print in a file and save whatever checks are required
+    to ensure the code ran correctly.
     Note that _log() will append the file if it already exists.
     
     Parameters
     ----------
-    - thing : Anything printable object
-    the 'thing' you want to print and log.
+    - thing : Any printable object
+    the 'thing' you want to print and log. Can be anything that you'd print with the print() function (string, list, Pandas DataFrame..)
     
     - logfile: String
     the full filename and path of the file where you want to log the results
@@ -21,14 +21,8 @@ def _log(thing, logfile):
     
     Examples
     --------
-    logfile = "/projects/loyalty/share/Big_W/201808_Lapsed/01_Code/01_Data_extraction.log"
-    # start logging
-    _log(datetime.now())
-    # Log anything
-    _log("Hello!"
-    _log(df.shape)
-    _proc_contents(df)
-
+    f = "./01_Code/01_import_data.log"
+    _log("Hello!", f)
     """
     f=open(logfile, "a+")
     print(thing, file=f)
